ee7734874f0b10966f10c20465c96196659c0531
rrduran <ronald.duran@redcross.org.ph>
2014-05-14 17:13:23 +0800

    automatic thumbnails but page styling problems

--- js/main.js
+++ js/main.js
@@ -1,45 +1,38 @@
+var mapData =[];
 
+function getmapList() {
+  $.ajax({
+    type: 'GET',
+    url: 'data/mapList.json',
+    contentType: 'application/json',
+    dataType: 'json',
+    timeout: 10000,
+    success: function(data) {
+      console.log("Success!");
+      mapData = data;
+      buildThumbnails();
 
-//generates html for preview boxes using data from maps.json
-function generatepreviewhtml(){
-    var html = "";
-    function formatDate(date){
-        var formattedDate = new Date(date).toString().substring(4,15);
-        return formattedDate;
+    },
+    error: function(e) {
+      console.log(e);
     }
-
-    $.each(mapData, function(index, item){              
-        var itemhtml = 
-            '<div id="'+item.map_id+'" style="display:none," class="thumbnailWrap col-sm-3 ALL-EXTENT ALL-SECTOR mapped '+item.extent+' '+item.sector+'">'+
-                '<div onclick="callModal(this);" class="thumbnail">'+
-                    '<img class="lazy" data-original="img/maps/'+item.fileName+'_thumb.jpg'+'" width="300" height="200" alt="" >'+
-                    '<div class="caption">'+            
-                        '<h5 style="font-weight:bold;">'+item.title+'</h5>'+
-                        '<p style="font-size:small; margin:6px 0 0 0;">' + formatDate(item.productionDate) +'</p>'+        
-                    '</div>'+
-                    '<div class="modalDescription" style="display:none;">'+                        
-                        '<h4 style="font-weight:bold;">'+item.title+' <small>('+formatDate(item.productionDate)+')</small></h4>'+                        
-                        '<p style="font-size:small; margin:6px 0 0 10px;">'+item.narrative+'</p>'+
-                        '<p style="font-size:small; margin:6px 0 0 10px;"><b>Extent tags:</b> '+item.extent.replace(/\s/g, ', ')+'</p>'+                         
-                        '<p style="font-size:small; margin:6px 0 0 10px;"><b>Type tags:</b> '+item.sector.replace(/\s/g, ', ')+'</p>'+                         
-                    '</div>'+   
-               '</div>'+
-            '</div>';
-        html=html+itemhtml;        
-        var itemExtents = item.extent.match(/\S+/g);
-        $.each(itemExtents, function(index, extent){
-            if (extentTags.indexOf(extent) === -1){
-                extentTags.push(extent);
-            }
-        });
-        var itemSectors = item.sector.match(/\S+/g);
-        $.each(itemSectors, function(index, sector){
-            if (sectorTags.indexOf(sector) === -1){
-                sectorTags.push(sector);
-            }
-        });
-    });
-    $('#gallery').html(html);
-    thumbnails = $("#gallery").children();
-    generateFilterButtons();
+  });
 }
+function buildThumbnails(){
+  $.each(mapData, function(index, thisMap ){
+    var thisHtml= "<div class='thumbnail col-sm-3 thumbnail-map'><img src='map/";
+    console.log(thisHtml);
+    thisHtml +=thisMap.filename;
+    console.log(thisHtml);
+    thisHtml += "_thumb.jpg'> <div class='caption'><h3>";
+    console.log(thisHtml);
+    thisHtml +=thisMap.label + "</h3> <p>" + thisMap.description + "</p> <p> <a target='_blank' href='map/";
+    thisHtml+=thisMap.filename + ".pdf'" + "class='btn btn-primary' role='button'>Download PDF</a></p></div></div>";
+    console.log(thisHtml);
+
+   $("#gallery").append(thisHtml);
+  });
+}
+
+
+ getmapList();

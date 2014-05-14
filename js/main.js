e2537588da25df79d20c449084755db81e6e46ce
Dan Joseph <danbjoseph@gmail.com>
2014-05-14 23:11:39 +0800

    page styling fix, pretty code, style dl buttons

--- js/main.js
+++ js/main.js
@@ -11,28 +11,23 @@ function getmapList() {
       console.log("Success!");
       mapData = data;
       buildThumbnails();
-
     },
     error: function(e) {
       console.log(e);
     }
   });
 }
+
 function buildThumbnails(){
   $.each(mapData, function(index, thisMap ){
-    var thisHtml= "<div class='thumbnail col-sm-3 thumbnail-map'><img src='map/";
+    var thisHtml = "<div class='thumbnail col-sm-3 thumbnail-map'><img src='map/";
+    thisHtml += thisMap.filename;
+    thisHtml += "_thumb.jpg'><div class='caption'><h3>";
+    thisHtml += thisMap.label + "</h3><p>" + thisMap.description + "</p><p><a target='_blank' href='map/";
+    thisHtml += thisMap.filename + ".pdf'" + "class='btn btn-default btn-downloadPdf' role='button'><span class='glyphicon glyphicon-download'></span> Download PDF</a></p></div></div>";
     console.log(thisHtml);
-    thisHtml +=thisMap.filename;
-    console.log(thisHtml);
-    thisHtml += "_thumb.jpg'> <div class='caption'><h3>";
-    console.log(thisHtml);
-    thisHtml +=thisMap.label + "</h3> <p>" + thisMap.description + "</p> <p> <a target='_blank' href='map/";
-    thisHtml+=thisMap.filename + ".pdf'" + "class='btn btn-primary' role='button'>Download PDF</a></p></div></div>";
-    console.log(thisHtml);
-
-   $("#gallery").append(thisHtml);
+    $("#gallery").append(thisHtml);
   });
 }
 
-
- getmapList();
+getmapList();

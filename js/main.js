f2aabf7243286603aca6a15dcd5bb576dc3ea4d9
rrduran <ronald.duran@redcross.org.ph>
2014-05-30 15:33:24 +0800

    Maps added

--- js/main.js
+++ js/main.js
@@ -21,14 +21,10 @@ function getmapList() {
 function buildThumbnails(){
   $.each(mapData, function(index, thisMap ){
     var thisHtml= "<div class='thumbnail col-sm-3 thumbnail-map'><img src='map/";
-    console.log(thisHtml);
     thisHtml +=thisMap.filename;
-    console.log(thisHtml);
     thisHtml += "_thumb.jpg'> <div class='caption'><h3>";
-    console.log(thisHtml);
-    thisHtml +=thisMap.label + "</h3> <p>" + thisMap.description + "</p> <p> <a target='_blank' href='map/";
-    thisHtml+=thisMap.filename + ".pdf'" + "class='btn btn-primary' role='button'>Download PDF</a></p></div></div>";
-    console.log(thisHtml);
+    thisHtml +=thisMap.label + "</h3> <p>" + thisMap.description + " <small>" +  thisMap.date + "</small></p> <p> <a target='_blank' href='map/";
+    thisHtml+=thisMap.filename + ".jpg'" + "class='btn btn-primary' role='button'>Download Map</a></p></div></div>";
 
    $("#gallery").append(thisHtml);
   });
@@ -36,3 +32,4 @@ function buildThumbnails(){
 
 
  getmapList();
+
